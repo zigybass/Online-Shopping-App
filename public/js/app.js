46b543c1964ba27ec74185266cfcfc3078f5e272
Zigy <zigybass@gmail.com>
2019-09-11 00:09:58 -0400

    shopCart needs work

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,7 @@
 $(document).ready( function() {
 
+
+    let shopCart = [];
     // $.get("/", function () {
 
     //     // renderProducts(req.body);
@@ -16,12 +18,12 @@ $(document).ready( function() {
             // console.log(dbProducts[i])
             $("#productsList").append(`
         
-            <div class="card" value="${dbProducts[i].price}" style="width: 18rem;">
+            <div class="card" style="width: 18rem;">
             <div class="card-body">
               <h5 class="card-title">${dbProducts[i].product_name}</h5>
               <p class="card-text">${dbProducts[i].Description}</p>
               <p>Price: $${dbProducts[i].price}</p>
-              <button value="${dbProducts[i].price}" class="btn btn-secondary btn-sm 
+              <button name="${dbProducts[i].product_name}" value="${dbProducts[i].price}" class="btn products btn-secondary btn-sm 
               ">Add to Cart</button>
             </div>
           </div>`
@@ -30,14 +32,30 @@ $(document).ready( function() {
     };
 
 
-    $(document).on("click", ".btn", function (e) {
+    $(document).on("click", ".products", function (e) {
         e.preventDefault();
-        console.log(e.currentTarget.value)
+        const name = e.currentTarget.name;
+        const price = e.currentTarget.value;
+        // console.log(e.currentTarget.name)
+        shopCart.push({[name]: price})
+        console.log(shopCart)
     })
 
+
+    // NEEDS FINISHING
     $("#cart").on("click", function (e) {
         e.preventDefault(); 
-        console.log("checkout")
+        console.log(shopCart);
+        let total;
+        for (i = 0; i < shopCart.length; i++) {
+            // let addPrice = parseInt(shopCart[i].price);
+            let numPrice = Object.values(shopCart[i])
+            // console.log(Object.values(shopCart[i]))
+            // total += parseInt(numPrice[0])
+            console.log( typeof numPrice )
+            console.log(numPrice)
+        }
+       console.log(total)
     })
 
 
