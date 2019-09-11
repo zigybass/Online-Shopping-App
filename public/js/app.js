30e33d96665d2aa13aeedac98c5217a997d6b038
Zigy <zigybass@gmail.com>
2019-09-10 23:15:02 -0400

    finally grabbing value for prices. need to add to cart

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,10 @@
 $(document).ready( function() {
 
-    $.get("/", function () {
+    // $.get("/", function () {
 
-        // renderProducts(req.body);
-    })
-
+    //     // renderProducts(req.body);
+    // })
+    // grabs db data and sends to render function
     $.get("/api/load", function (data) {
         console.log(data)
         renderProducts(data)
@@ -14,14 +14,15 @@ $(document).ready( function() {
     function renderProducts (dbProducts) {
         for (i = 0; i < dbProducts.length; i++ ) {
             // console.log(dbProducts[i])
-            $("#products").append(`
-            
-            <div class="card" style="width: 18rem;">
+            $("#productsList").append(`
+        
+            <div class="card" value="${dbProducts[i].price}" style="width: 18rem;">
             <div class="card-body">
               <h5 class="card-title">${dbProducts[i].product_name}</h5>
               <p class="card-text">${dbProducts[i].Description}</p>
               <p>Price: $${dbProducts[i].price}</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
+              <button value="${dbProducts[i].price}" class="btn btn-secondary btn-sm 
+              ">Add to Cart</button>
             </div>
           </div>`
             )
@@ -29,18 +30,19 @@ $(document).ready( function() {
     };
 
 
-    $("#products").on("click", function (e) {
+    $(document).on("click", ".btn", function (e) {
         e.preventDefault();
-        console.log("Click: " + e.currentTarget.val())
+        console.log(e.currentTarget.value)
+    })
 
-    $("#cart").on("click", function () {
+    $("#cart").on("click", function (e) {
+        e.preventDefault(); 
         console.log("checkout")
     })
 
 
-})
+
 $('#modal').on('shown.bs.modal', function () {
     $('#myInput').trigger('focus')
   })
-
 })
